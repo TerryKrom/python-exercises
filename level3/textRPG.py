613a5aa96aa58835761f026bca214c7769cac104
TerryKrom <terrykrom1@gmail.com>
2023-10-24 22:09:59 -0300

    textRPG updated

---
+++ level3/textRPG.py
@@ -0,0 +1,133 @@
+import os
+import random
+
+icons = {
+    'heart': '\u2764\uFE0F',
+    'swords': '\u2694\uFE0F',
+    'shield': '\U0001f6e1\uFE0F'
+}
+
+class Hero:
+    def __init__(self, name):
+        self.life = 20
+        self.damage = 8
+        self.armor = 10
+        self.actions = ["attack", "defense", "items", "check", "run"]
+        self.items = []
+        self.name = name
+        
+    def checkItems(self):
+        if len(self.items) > 0:
+            for item in self.items:
+                print(item)
+        else:
+            print('Empty bag!')
+        
+class Monster:
+    levels = {
+        'beginner': {
+            'names': ['slime', 'goblin', 'rat'],
+            'life': 10,
+            'damage': 5,
+            'armor': 3
+        },
+        'medium': {
+            'names': ['big slime', 'skeleton', 'thief'],
+            'life': 16,
+            'damage': 8,
+            'armor': 6
+        },
+        'advanced': {
+            'names': ['giant slime', 'armored skeleton', 'warrior'],
+            'life': 20,
+            'damage': 10,
+            'armor': 8
+        },
+    }
+    def __init__(self, level):
+        self.life = self.levels[level]['life']
+        self.damage = self.levels[level]['damage']
+        self.armor = self.levels[level]['armor']
+        self.name = random.choice(self.levels[level]['names'])
+        
+class Game:
+    def __init__(self):
+        print('- Welcome to Text RPG!')
+        print()
+        print("- Let's begin our adventure")
+        print("- What is the name of our hero? ")
+        print()
+        name = input('Your answer: ')
+        if name == '':
+            name = 'Hero'
+        self.clear()
+        print(f"Wonderful! nice to meet you, {name}")
+        self.hero = Hero(name)
+        print('- So...')
+        print('- You will start your journey, lost inside of a cave')
+        print("- You don't remember anything... ")
+        print()
+        print("Press enter to begin")
+        code = input()
+        if code == '':
+            self.startGame()
+    
+    def startGame(self):
+        self.clear()
+        self.printMenu()
+        self.createMonster()
+        self.printActions()
+        self.heroTurn()
+        
+    def printMenu(self):
+        name = self.hero.name
+        hp = self.hero.life
+        damage = self.hero.damage
+        armor = self.hero.armor
+        print('='*8+f' {name} '+'='*8)
+        stats = f'{icons["heart"]} {hp} {icons["swords"]} {damage}  {icons["shield"]} {armor}'
+        print(stats.center(26, ' '))
+        
+    def printActions(self):
+        print()
+        actions = self.hero.actions
+        for act in actions:
+            print(f'[{act[0]}] {act}')
+        
+    def createMonster(self):
+        print()
+        self.monster = Monster('beginner')
+        print(f'- A {self.monster.name} appears!')
+        
+    def clear(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+    
+    def checkMonster(self):
+        self.clear()
+        print('Stats: ')
+        print(f'{icons["heart"]} {self.monster.life}')
+        print(f'{icons["swords"]} {self.monster.damage}')
+        print(f'{icons["shield"]} {self.monster.armor}')
+        print()
+        print('Press enter to return')
+        code = input()
+        if code == '':
+            self.startGame()
+    
+    def heroTurn(self):
+        while True:
+            action = input('Choose an action: ')
+            if action in self.hero.actions:
+                if action == 'a':
+                    self.heroAttack()
+                elif action == 'd':
+                    self.heroDefense()
+                elif action == 'i':
+                    self.hero.checkItems()
+                elif action == 'c':
+                    self.checkMonster()
+                elif action == 'r':
+                    self.runAway()
+                break
+
+game = Game()
